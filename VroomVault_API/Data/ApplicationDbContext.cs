8fb4aa4ac02a4c9e7b953fef7109191a991d3d8a
Jeremiah E. Wright <jeremiahewdev@gmail.com>
2023-09-20 21:53:25 -0400

    add sql migration

---
+++ VroomVault_API/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VroomVault_API.Models.Car;
+
+namespace VroomVault_API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+   : base(options)
+        {
+
+        }
+        public DbSet<CarBrand> CarBrands { get; set; }
+    }
+
+}
